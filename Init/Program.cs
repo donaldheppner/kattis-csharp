015d9d44dc878302bf237fc58221de86bf3a15f2
Donald Heppner <donald.heppner@gmail.com>
2017-10-20 17:45:22 -0400

    Got the inital test harness working; solving for carrots.

---
+++ Init/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+// ReSharper disable once CheckNamespace
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solve(Console.OpenStandardInput(), Console.OpenStandardOutput());
+    }
+
+    public static void Solve(Stream stdin, Stream stdout)
+    {
+        var reader = new StreamReader(stdin);
+        var writer = new StreamWriter(stdout);
+
+        var answer = reader.ReadLine()?.Split(' ').Last() ?? string.Empty;
+
+
+        writer.WriteLine(answer);
+        writer.Flush();
+    }
+}
